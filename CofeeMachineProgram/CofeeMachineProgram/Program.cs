a8f372da38c9d6e0fb01da2f7051c549ce5fcbc3
Hamlet Poghosian <phamik@mail.ru>
2019-04-15 15:42:11 +0400

    Add new Fuction

--- CofeeMachineProgram/CofeeMachineProgram/Program.cs
+++ CofeeMachineProgram/CofeeMachineProgram/Program.cs
@@ -11,45 +11,53 @@ namespace CofeeMachineProgram
 		static void Main(string[] args)
 		{
 			int coin = 0;
-		    int mycoin=0;
+		    int myCoin=0;
+			bool myhoice = true;
 			Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins \n");
 			Console.Write("This is Coffee type and their prices \n1-coffe = 100  \n2-maccofe  = 200 \n3-strong coffe = 450 \n");
-			Console.WriteLine("Please throw your coin or Enter 0 and  select Coffee");
 
-			while (true)
+			while (myhoice)
 			{
+			 Console.WriteLine("Please throw your coin or Enter 0 and  select Coffee");
 				try
 				{
 					coin = int.Parse(Console.ReadLine());
 					switch (coin)
 					{
+						case 0:
+							{
+								
+								Console.WriteLine("You have {0} coin", myCoin);
+								myhoice = false;
+								break;
+							}
 						case 50:
 							{
-								mycoin += 50;
-								Console.WriteLine("You have {0} coin", mycoin);
+								myCoin += 50;
+								Console.WriteLine("You have {0} coin", myCoin);
 								break;
 							}
 						case 100:
 							{
-								mycoin += 100;
-								Console.WriteLine("You have {0} coin", mycoin);
+								myCoin += 100;
+								Console.WriteLine("You have {0} coin", myCoin);
 								break;
 							}
 						case 200:
 							{
-								mycoin += 200;
-								Console.WriteLine("You have {0} coin", mycoin);
+								myCoin += 200;
+								Console.WriteLine("You have {0} coin", myCoin);
 								break;
 							}
 						case 500:
 							{
-								mycoin += 500;
-								Console.WriteLine("You have {0} coin", mycoin);
+								myCoin += 500;
+								Console.WriteLine("You have {0} coin", myCoin);
 								break;
 							}
 						default:
 							{
-								Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins \n");
+								Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins  \n");
 								break;
 							}
 					}
