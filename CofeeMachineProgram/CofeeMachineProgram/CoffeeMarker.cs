7aae9114c3f24e6379ca7f05e862873d8828c98b
HamletPoghosian <phamik@mail.ru>
2019-04-16 13:20:34 +0400

    Add new class and Fuction

---
+++ CofeeMachineProgram/CofeeMachineProgram/CoffeeMarker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CofeeMachineProgram
+{
+    public enum CoffeeNumber
+    {
+        Americano = 1,
+        Latte = 2,
+        Cappuccino = 3,
+        Espresso = 4,
+        Macchiato = 5,
+        Vienna = 6,
+        Affogato = 7,
+        Doppio = 8,
+        Ristretto = 9,
+        Maccoffee = 10
+    }
+    public class CoffeeMarker
+    {
+        public CoffeeNumber CoffeeType { get; set; }
+        public CoffeeMarker(int coffenumber)
+        {
+
+            CoffeeType = (CoffeeNumber)coffenumber;
+        }
+    }
+}
--- CofeeMachineProgram/CofeeMachineProgram/Program.cs
+++ CofeeMachineProgram/CofeeMachineProgram/Program.cs
@@ -6,80 +6,129 @@ using System.Threading.Tasks;
 
 namespace CofeeMachineProgram
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int coin = 0;
-		        int myCoin=0;
-			bool myhoice = true;
-			Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins \n");
-			Console.Write("This is Coffee type and their prices \n1-coffe = 100  \n2-maccofe  = 200 \n3-strong coffe = 450 \n");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin = 0;
+            int myCoin = 0;
+            bool isThrow = true;
 
-			while (myhoice)
-			{
-			 Console.WriteLine("Please throw your coin or Enter 0 and  select Coffee");
-				try
-				{
-					coin = int.Parse(Console.ReadLine());
-					switch (coin)
-					{
-						case 0:
-							{
-								
-								Console.WriteLine("You have {0} coin", myCoin);
-								myhoice = false;
-								break;
-							}
-						case 50:
-							{
-								myCoin += 50;
-								Console.WriteLine("You have {0} coin", myCoin);
-								break;
-							}
-						case 100:
-							{
-								myCoin += 100;
-								Console.WriteLine("You have {0} coin", myCoin);
-								break;
-							}
-						case 200:
-							{
-								myCoin += 200;
-								Console.WriteLine("You have {0} coin", myCoin);
-								break;
-							}
-						case 500:
-							{
-								myCoin += 500;
-								Console.WriteLine("You have {0} coin", myCoin);
-								break;
-							}
-						default:
-							{
-								Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins  \n");
-								break;
-							}
-					}
-				}
-				catch (ArgumentNullException)
-				{
+            Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins \nAnd  then Enter 0 and select Coffee type \n");
+            while (isThrow)
+            {
+                Console.WriteLine("Please throw your coin");
+                try
+                {
+                    coin = int.Parse(Console.ReadLine());
+                    switch (coin)
+                    {
+                        case 0:
+                            {
 
-					Console.WriteLine("Please enter coin");
-				}
-				catch (FormatException )
-				{
+                                Console.WriteLine("You have {0} coin", myCoin);
+                                isThrow = false;
+                                break;
+                            }
+                        case 50:
+                            {
+                                myCoin += 50;
+                                Console.WriteLine("You have {0} coin", myCoin);
+                                break;
+                            }
+                        case 100:
+                            {
+                                myCoin += 100;
+                                Console.WriteLine("You have {0} coin", myCoin);
+                                break;
+                            }
+                        case 200:
+                            {
+                                myCoin += 200;
+                                Console.WriteLine("You have {0} coin", myCoin);
+                                break;
+                            }
+                        case 500:
+                            {
+                                myCoin += 500;
+                                Console.WriteLine("You have {0} coin", myCoin);
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Write("Please throw a coin. \nYou can only throw (50, 100, 200, 500) coins Or Enter 0 and select Coffee Type \n");
+                                break;
+                            }
+                    }
+                }
+                catch (ArgumentNullException)
+                {
 
-					Console.WriteLine("Please enter 50,100,200 and 500 ");
-				}				
-				catch (Exception)
-				{
+                    Console.WriteLine("Please enter coin or Enter 0 and select Coffee");
+                }
+                catch (FormatException)
+                {
 
-					throw;
-				}
-				
-			}
+                    Console.WriteLine("Please enter 50,100,200 and 500 or Enter 0 and select Coffee");
+                }
+                catch (Exception)
+                {
 
-		}
-	}
+                    throw;
+                }
+
+            }
+            Console.WriteLine("Please enter Coffee number ");
+            Console.Write(@"This is Coffee type and their prices 
+1.Americano    50 (: 10 water, 10 sugar, 10 coffee)
+2.Latte        100 (: 10 water, 20 sugar, 10 coffee)
+3.Cappuccino   250 (: 10 water, 20 sugar, 20 coffee)
+4.Espresso     300 (: 20 water, 10 sugar, 20 coffee)
+5.Macchiato    450 (: 20 water, 20 sugar, 20 coffee)
+6.Vienna       550 (: 30 water, 10 sugar, 40 coffee)
+7.Affogato     700 (: 40 water, 40 sugar, 40 coffee)
+8.Doppio       800 (: 20 water, 50 sugar, 50 coffee)
+9.Ristretto    900 (: 50 water, 50 sugar, 30 coffee)
+10.Maccoffee   1000(: 50 water, 50 sugar, 50 coffee)");
+            Console.WriteLine();
+            bool selectCoffe = true;
+            int coffeeType = 0;
+            Console.WriteLine("Enter Coffee  number");
+            while (selectCoffe)
+            {
+                try
+                {
+                    if (myCoin < 50)
+                    {
+                        selectCoffe = false;
+                        Console.WriteLine("Your many end ");
+                        break;
+                    }
+                    coffeeType = int.Parse(Console.ReadLine());
+                    if (coffeeType >= 1 && coffeeType <= 10)
+                    {
+                        CoffeeMarker cofMarker = new CoffeeMarker(coffeeType);
+                        Console.WriteLine(cofMarker.CoffeeType);
+                    }
+                }
+                catch (ArgumentNullException)
+                {
+
+                    Console.WriteLine("1-10 number");
+                }
+                catch (FormatException)
+                {
+
+                    Console.WriteLine("1-10 number");
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+
+            }
+        }
+    }
 }
+
